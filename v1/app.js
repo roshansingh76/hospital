08710b6b2a09a7c18c0a8f82c39c63b2590a5ded
roshan singh <roshansingh9450@gmail.com>
2020-02-12 00:25:20 +0530

    app changes

--- v1/app.js
+++ v1/app.js
@@ -4,28 +4,17 @@ const path = require('path');
 var bodyParser = require('body-parser');
 const db = require('./config/database');
 const users = require('./routes/users');
-
-<<<<<<< HEAD
-// Initialize express app
-const app = express();
-
-=======
 //Ankur code
 var app = express();
-app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
-}));
+
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
->>>>>>> 048dcb27d3d77f5568841fc69fb05081e582a1a7
+//app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
