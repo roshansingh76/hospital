0ea4fa8d4ea865a8a75458c9daddbe9ff408566b
ankur <rockingankur4u@gmail.com>
2020-02-23 20:43:08 +0530

    Add Phono Validation

--- v1/routes/user.js
+++ v1/routes/user.js
@@ -2,7 +2,7 @@ var express =require('express');
 var router = express.Router();
 const { check, validationResult } = require('express-validator');
 var db = require('../config/database');
-var User = require('../controllers/userController');
-router.get('/getOtp',User.getOtp);
-
+var Auth = require('../controllers/authController');
+router.get('/getOtp',Auth.getOtp);
+router.get('/verifyOtp',Auth.verifyOtp);
 module.exports = router;
