b14a0279e52767ed208af9695db28e2508c7c2e0
susil <susilbhr@gmail.com>
2020-02-24 10:59:56 +0530

    style update and left list bar changes

--- src/components/Talkastrologer/leftnav.js
+++ src/components/Talkastrologer/leftnav.js
@@ -10,63 +10,17 @@ const TLeftnav=()=>(
 	
 	<div className="col-md-3">
 			<div className="astro-left-filt">
-				<div className="ser-title-top">
-					<h3><a href="#myModals" data-toggle="modal"><i className="fa fa-filter"></i> Filter</a></h3>
-				</div>
-			
-				<div className="bs-filter hidden-xs">
-					<div className="panel-group" id="accordion">
-						<div className="panel panel-default">
-							<div className="panel-heading">
-								<h4 className="panel-title">
-									<a href="#1"><span className="glyphicon glyphicon-plus"></span> Astrologer</a>
-								</h4>
-							</div>
-						</div>
-						<div className="panel panel-default">
-							<div className="panel-heading">
-								<h4 className="panel-title">
-									<a href="#2"><span className="glyphicon glyphicon-plus"></span> Get Reports </a>
-								</h4>
-							</div>							
-						</div>
-						<div className="panel panel-default">
-							<div className="panel-heading">
-								<h4 className="panel-title">
-									<a href="#2"><span className="glyphicon glyphicon-plus"></span> Shubh Muhurat </a>
-								</h4>
-							</div>							
-						</div>
-						
-						
-						<div className="panel panel-default">
-							<div className="panel-heading">
-								<h4 className="panel-title">
-									<a data-toggle="collapse" data-parent="#accordion" href="#3"><span className="glyphicon glyphicon-plus"></span> Micro Vastu </a>
-								</h4>
-							</div>					
-						</div>
-						
-						<div className="panel panel-default">
-							<div className="panel-heading">
-								<h4 className="panel-title">
-									<a data-toggle="collapse" data-parent="#accordion" href="#4"><span className="glyphicon glyphicon-plus"></span> Handmade Kundli </a>
-								</h4>
-							</div>							
-						</div>
-						
-						<div className="panel panel-default">
-							<div className="panel-heading">
-								<h4 className="panel-title">
-									<a data-toggle="collapse" data-parent="#accordion" href="#4"><span className="glyphicon glyphicon-plus"></span> Free Kundli  </a>
-								</h4>
-							</div>							
-						</div>
-						
-						
-					</div>
-				</div>
-			</div>
+				<p>Our Services</p>
+				<ul>
+					<li><a href="#" className="active">Astrologer  <i className="fa fa-bell-o" aria-hidden="true"></i></a></li>
+					<li><a href="#" >Get Reports</a></li>
+					<li><a href="#" >Shubh Muhurat</a></li>
+					<li><a href="#" >Micro Vastu</a></li>
+					<li><a href="#" >Handmade Kundli</a></li>
+					<li><a href="#" >Free Kundli</a></li>
+
+				</ul>
+			</div>	
 		</div>
 );
 export default TLeftnav;
