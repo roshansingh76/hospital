0dd5c91568dc728e687fd983ac6e512bda485766
susil <susilbhr@gmail.com>
2020-02-25 19:13:30 +0530

    package updat,stylesheet updat

--- src/components/Pack/callpacking.js
+++ src/components/Pack/callpacking.js
@@ -8,9 +8,89 @@ class Callingpack extends Component {
    }
   render() {
 		return (
-		  <div>
-			<h1>Calling Pack</h1>
-		  </div>
+		  		<section className="padding">
+		  			<div className="container">
+		  				<h1>Add money to your wallet</h1>
+		  				<div className="row">
+		  					<div className="col-md-8">
+		  						<div className="row">
+
+		  							<div className="col-md-6">
+		  								<a href="#">
+					  						<div className="asto-package-price">
+					  							<p>50 <i className="fa fa-inr"></i></p>
+					  							<span>Add money</span>
+					  						</div>
+				  						</a>
+				  					</div>
+				  					
+				  				</div>
+		  					</div>
+
+
+		  					<div className="col-md-4">
+		  						<div className="cart-total">
+		  							<p>Total Money Add</p>
+
+		  							<div className="mony-info">
+										<table class="table cart mt-10">
+											<tbody>
+											    <tr class="cart_item cart_totle">
+											        <td class="cart-product-name">
+											            <strong>Price Add</strong>
+											        </td>
+											        <td class="cart-product-name">
+											            <span class="amount">1000 <i className="fa fa-inr"></i></span>
+											        </td>
+											    </tr>
+											    <tr class="cart_item cart_totle">
+											        <td class="cart-product-name">
+											            <strong>GST@18%</strong>
+											        </td>
+											        <td class="cart-product-name">
+											            <span class="amount">20 <i className="fa fa-inr"></i></span>
+											        </td>
+											    </tr>
+											    <tr class="cart_item cart_totle">
+											        <td class="cart-product-name">
+											            <strong>Discount</strong>
+											        </td>
+											        <td class="cart-product-name">
+											            <span class="theme"><strong>- 50</strong> <i className="fa fa-inr"></i></span>
+											        </td>
+											    </tr>
+											    <tr class="cart_item cart_totle">
+											        <td class="cart-product-name">
+											            <strong>Total Payable Amount</strong>
+											        </td>
+											        <td class="cart-product-name">
+											            <span class="theme"><strong><i className="fa fa-inr"></i>1020</strong></span>
+											        </td>
+											    </tr>
+											</tbody>
+										</table>
+		  							</div>
+
+		  							<div className="referl-info">
+		  								<p>Do you have referral code?</p>
+		  								<form class="form-inline">
+		  									<div class="form-group">
+										      <input type="email" class="form-control" placeholder="Enter Referal Code" name="email" />
+										      <button type="submit" class="btn btn-default">Apply</button>
+										    </div>
+		  								</form>
+		  							</div>
+
+		  							<div className="addmoney-btn">
+										<button type="submit" class="btn btn-default">Proceed to Recharge</button>
+		  							</div>
+
+		  						</div>
+		  					</div>
+
+		  				</div>
+		  			</div>
+		  		</section>
 		);
   }
 }
