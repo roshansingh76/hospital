68f0c2601ba57478335ad6e3a00c146f5c11ac1a
ankur <rockingankur4u@gmail.com>
2020-02-29 12:16:07 +0530

    Add Top Astrologer Api

--- v1/controllers/serviceController.js
+++ v1/controllers/serviceController.js
@@ -97,3 +97,26 @@ exports.getPackages = function(req, res) {
 	})
 }
 
+exports.getTopAstrologers = function(req, res) {	
+	let sql="SELECT * FROM `users` ORDER BY RAND() LIMIT 5";
+	db.query(sql,function(err,result){
+		if(err){
+			res.status(401).json({
+				success: false,
+				data:err,
+				message:'Error in getting packages!'
+			});
+		}
+	})
+}
+
+exports.getTopAstrologers = function(req, res) {
+	let sql="SELECT *,(SELECT GROUP_CONCAT(l.language_name) FROM `language` l WHERE l.id IN (SELECT ln.language_id FROM `astro_languages` ln WHERE  ln.user_id = u.id)) as language_name,(SELECT GROUP_CONCAT(e.expertise_name) FROM `expertise` e WHERE e.id IN (SELECT ue.expertise_id FROM `astro_expertise` ue WHERE  ue.user_id = u.id)) as expertise_name from users u where u.cb_roles_id=2 ORDER BY RAND() LIMIT 5";
+		db.query(sql,function(err,result){
+			if(err) throw err;
+			res.status(200).json({
+				success: true,
+				result:result
+			});	
+		})
+}
