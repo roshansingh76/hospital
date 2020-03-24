de7b09e60f6b8d5e4bd2186131051744c1cee05e
Pritam Kumar <pk.rajece2010@gmail.com>
2020-03-24 12:37:19 +0530

    Added user updation after signup

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,103 +1,118 @@
-import React from 'react';
-import { BrowserRouter as Router, Route,Redirect,withRouter,useHistory,Switch } from 'react-router-dom';
-import deviceStorage from '../../config/deviceStorage';
-import * as ROUTES from '../../config/routes';
-import Navigation from '../Navigation';
-import Sidebar from '../Navigation/Sidebar';
-import FrontendTopbar from '../Navigation/frontend/nav';
-import FrontendHeader from '../Navigation/frontend/FrontendHeader';
-import FrontendNavigation from '../Navigation/frontend/Frontendnavigation';
-import Footer from '../Navigation/frontend/Mainfooter';
-import HomePage from '../Home';
-import LoginPage from '../Login'
-import AdminDashboard from '../AdminDashboard'
-import AstrologerPage from '../Astrologer'
-import CreateAstro from '../Astrologer/create'
-import SettingPage from '../Setting'
-import Talkastrologer from '../Talkastrologer'
-import Talkastrologerdetail from '../Talkastrologer/detail'
-import Muhurat from '../Muhurat'
-import Vastu from '../Vastu'
-import Kundli from '../Kundli'
-import Horoscope from '../Horoscope'
-import Chat from '../communication/chat'
-import Call from '../communication/call'
-import Report from '../communication/report'
-import About from '../About'
-import Blogs from '../Blogs'
-import Callingpack from '../Pack/callpacking'
-import MuhuratDeatil from  '../Muhurat/muhuratsingle'
-import VastuDetail from  '../Vastu/vastusingle'
-import KundliDetail from  '../Kundli/kundlisingle'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  withRouter,
+  useHistory,
+  Switch
+} from "react-router-dom";
+import deviceStorage from "../../config/deviceStorage";
+import * as ROUTES from "../../config/routes";
+import Navigation from "../Navigation";
+import Sidebar from "../Navigation/Sidebar";
+import FrontendTopbar from "../Navigation/frontend/nav";
+import FrontendHeader from "../Navigation/frontend/FrontendHeader";
+import FrontendNavigation from "../Navigation/frontend/Frontendnavigation";
+import Footer from "../Navigation/frontend/Mainfooter";
+import HomePage from "../Home";
+import LoginPage from "../Login";
+import AdminDashboard from "../AdminDashboard";
+import AstrologerPage from "../Astrologer";
+import CreateAstro from "../Astrologer/create";
+import SettingPage from "../Setting";
+import Talkastrologer from "../Talkastrologer";
+import Talkastrologerdetail from "../Talkastrologer/detail";
+import Muhurat from "../Muhurat";
+import Vastu from "../Vastu";
+import Kundli from "../Kundli";
+import Horoscope from "../Horoscope";
+import Chat from "../communication/chat";
+import Call from "../communication/call";
+import Report from "../communication/report";
+import About from "../About";
+import Blogs from "../Blogs";
+import Callingpack from "../Pack/callpacking";
+import MuhuratDeatil from "../Muhurat/muhuratsingle";
+import VastuDetail from "../Vastu/vastusingle";
+import KundliDetail from "../Kundli/kundlisingle";
 
-import UserAccountPage from  '../Users'
+import UserAccountPage from "../Users";
 
-const App= (props) => {
-	const isAuthenticated = localStorage.getItem('token');
-	const isPrivilage = localStorage.getItem('isPrivilage');
-	const location= window.location.pathname;
-	console.log();
-	if(isAuthenticated && isPrivilage==='Admin'){
-	return (
-		<div className="main-wrapper" >
-		 <Router>
-		   <Navigation />	
-		   <Sidebar />	
-		   		<Route   path={ROUTES.AdminDashboard} component={AdminDashboard} />
-				<Route   path={ROUTES.Astrologer} component={AstrologerPage} />
-				<Route   path={ROUTES.addAstrologer} component={CreateAstro} />
-				<Route   path={ROUTES.Setting} component={SettingPage} />
-		 </Router>
-	 </div>
-	);
-  
-  }else if(location==='/adminlogin' && !isAuthenticated){
-	  return (
-		<div className="main-wrapper" >
-		 <Router>
-			<Redirect to={ROUTES.ADMIN_LOGIN_IN}/>
-			<Route   path={ROUTES.ADMIN_LOGIN_IN} component={LoginPage} />	
-		 </Router>
-	  </div>)
-  }else{
-	  return (
-		<div className="main-wrapper" >
-		 <Router>
-		   <div className="educare-header-section top-bar jy-header "> 
-				<FrontendTopbar {...props}/>
-				<FrontendHeader {...props}/>
-				<FrontendNavigation {...props}/>
-		   </div>
-		   	<Route   exact path={ROUTES.Home} component={HomePage} />	
-		
-			<Route   path={ROUTES.Talkastrologer} component={Talkastrologer} />
-			<Route   path={ROUTES.Astrodetails} component={Talkastrologerdetail} />
-			<Route   path={ROUTES.Callingpack} component={Callingpack} />
-			<Route   path={ROUTES.Chat} component={Chat} />
-			<Route   path={ROUTES.Call} component={Call} />
-			<Route   path={ROUTES.Report} component={Report} />
-			<Route   path={ROUTES.About} component={About} />
-			<Route   path={ROUTES.Blogs} component={Blogs} />
-			<Switch>
-			<Route   path={ROUTES.Vastudetals+':slug'} component={VastuDetail} />
-			<Route   path={ROUTES.Muhuratdetails+':slug'} component={MuhuratDeatil} />
-			<Route   path={ROUTES.Kundlidetails+':slug'} component={KundliDetail} />
-			
-			<Route   path={ROUTES.VastuList} component={Vastu} />
-			<Route   path={ROUTES.MuhuratList} component={Muhurat} />
-			<Route   path={ROUTES.KundliList} component={Kundli} />
-			</Switch>
-			<Route   path={ROUTES.Horoscopes+"/:slug"} component={Horoscope} />	
-			
-			<Route   path={ROUTES.Horoscopes} component={Horoscope} />	
-			<Route   path={ROUTES.UserAccount} component={UserAccountPage} />	
+const App = props => {
+  const isAuthenticated = localStorage.getItem("token");
+  const isPrivilage = localStorage.getItem("isPrivilage");
+  const location = window.location.pathname;
+  console.log();
+  if (isAuthenticated && isPrivilage === "Admin") {
+    return (
+      <div className="main-wrapper">
+        <Router>
+          <Navigation />
+          <Sidebar />
+          <Route path={ROUTES.AdminDashboard} component={AdminDashboard} />
+          <Route path={ROUTES.Astrologer} component={AstrologerPage} />
+          <Route path={ROUTES.addAstrologer} component={CreateAstro} />
+          <Route path={ROUTES.Setting} component={SettingPage} />
+        </Router>
+      </div>
+    );
+  } else if (location === "/adminlogin" && !isAuthenticated) {
+    return (
+      <div className="main-wrapper">
+        <Router>
+          <Redirect to={ROUTES.ADMIN_LOGIN_IN} />
+          <Route path={ROUTES.ADMIN_LOGIN_IN} component={LoginPage} />
+        </Router>
+      </div>
+    );
+  } else {
+    return (
+      <div className="main-wrapper">
+        <Router>
+          <div className="educare-header-section top-bar jy-header ">
+            <FrontendTopbar {...props} />
+            <FrontendHeader {...props} />
+            <FrontendNavigation {...props} />
+          </div>
+          <Route exact path={ROUTES.Home} component={HomePage} />
 
-			
-			
-			<Footer {...props }/>
-		 </Router>
-	  </div>)
+          <Route path={ROUTES.Talkastrologer} component={Talkastrologer} />
+          <Route path={ROUTES.Astrodetails} component={Talkastrologerdetail} />
+          <Route path={ROUTES.Callingpack} component={Callingpack} />
+          <Route path={ROUTES.Chat} component={Chat} />
+          <Route path={ROUTES.Call} component={Call} />
+          <Route path={ROUTES.Report} component={Report} />
+          <Route path={ROUTES.About} component={About} />
+          <Route path={ROUTES.Blogs} component={Blogs} />
+          <Switch>
+            <Route
+              path={ROUTES.Vastudetals + ":slug"}
+              component={VastuDetail}
+            />
+            <Route
+              path={ROUTES.Muhuratdetails + ":slug"}
+              component={MuhuratDeatil}
+            />
+            <Route
+              path={ROUTES.Kundlidetails + ":slug"}
+              component={KundliDetail}
+            />
+
+            <Route path={ROUTES.VastuList} component={Vastu} />
+            <Route path={ROUTES.MuhuratList} component={Muhurat} />
+            <Route path={ROUTES.KundliList} component={Kundli} />
+          </Switch>
+          <Route path={ROUTES.Horoscopes + "/:slug"} component={Horoscope} />
+
+          <Route path={ROUTES.Horoscopes} component={Horoscope} />
+          <Route path={ROUTES.UserAccount} component={UserAccountPage} />
+
+          <Footer {...props} />
+        </Router>
+      </div>
+    );
   }
-}
+};
 
 export default App;
---
+++ src/components/UserInformation.js
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
+
+const UserInformation = props => {
+  const [modalIsOpen, setIsOpen] = React.useState(true);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("male");
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  const handleInputChange = event => {
+    if (event.target.name === "name") setName(event.target.value);
+    if (event.target.name === "email") setEmail(event.target.value);
+  };
+  const handleRadionButton = event => {
+    setGender(event.target.value);
+  };
+  const handleFormSubmit = event => {
+    if (name && email && gender) {
+      props.updateUser({
+        name,
+        email,
+        gender,
+        token: localStorage.getItem("token")
+      });
+    }
+  };
+  const { loader, error, hasErrored } = props;
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      contentLabel="Example Modal"
+    >
+      <div style={{ maxWidth: "500px", padding: "20px" }}>
+        <a className="model-close-cust" onClick={closeModal}>
+          <i className="fa fa-times"></i>
+        </a>
+
+        <h3>Please share the details</h3>
+        {hasErrored && (
+          <div class="alert alert-danger" role="alert">
+            {error}
+          </div>
+        )}
+        <form style={{ padding: "20px" }}>
+          <input
+            type="text"
+            autoComplete="off"
+            placeholder="Name"
+            className="form-control form-control-l-t"
+            name="name"
+            style={{ marginBottom: "15px" }}
+            value={name}
+            onChange={handleInputChange}
+          />
+          <input
+            type="email"
+            autoComplete="off"
+            placeholder="Email"
+            className="form-control form-control-r-t"
+            style={{ marginBottom: "15px" }}
+            name="email"
+            value={email}
+            onChange={handleInputChange}
+          />
+          <div class="form-check form-check-inline">
+            <input
+              class="form-check-input"
+              type="radio"
+              name="gender"
+              id="male"
+              value="male"
+              checked={gender === "male"}
+              onChange={handleRadionButton}
+            />
+            <label class="form-check-label" for="male">
+              Male
+            </label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input
+              class="form-check-input"
+              type="radio"
+              name="gender"
+              id="female"
+              value="female"
+              checked={gender === "female"}
+              onChange={handleRadionButton}
+            />
+            <label class="form-check-label" for="female">
+              Female
+            </label>
+          </div>
+          <button
+            type="button"
+            class="btn mob-btn btn-block"
+            disabled={loader}
+            style={{ marginTop: "15px" }}
+            onClick={handleFormSubmit}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </Modal>
+  );
+};
+
+export default UserInformation;
