056a3fd17f293a26d7c4012f85001bf4ad0156a1
susil <susilbhr@gmail.com>
2020-03-26 16:23:05 +0530

    style update andastloger detail page update

--- src/components/Talkastrologer/detail.js
+++ src/components/Talkastrologer/detail.js
@@ -9,104 +9,208 @@ class Astrologerdetails extends Component{
 	render(){
 		
 			return(
-				<section className="bg-ast">
+	<section className="bg-ast">
 		<div className="container">
 			<div className="row bg-ast-pd">
-			<div className="col-md-3"></div>
-				<div className="col-md-12">
-					<div className="j-astro-profile">
-						<div className="profile-pic-j">
-							<img src="https://astrotalk.com/assets/astro/771.jpg" alt="image" />
-						</div>
-						<div className="profile-info-j">
-							<h3>Acharya Prabhat</h3>
-							
-							<span className="a-spc">Speciality</span>
-							<span className="a-spc-info">Vastu Consultant, Astrologer, Gems Stone</span>
-							
-							<span className="a-exp">Experience</span>
-							<span className="a-exp-info">10 Year</span>
-							
-							<span className="a-edc">Education</span>
-							<span className="a-edc-info">PHD in Astrology and Horoscope</span>
-							
-							<span className="a-lan">Languages</span>
-							<span className="a-lan-info">Hindi, English</span>
-							
-							<span className="a-rat">Rating</span>
-							<span className="a-rat-info">
-								<span className="fa fa-star checked"></span>
-								<span className="fa fa-star checked"></span>
-								<span className="fa fa-star checked"></span>
-								<span className="fa fa-star checked"></span>
-								<span className="fa fa-star checked"></span>
-							</span>
-							
-							<span className="a-loc">Location</span>
-							<span className="a-loc-info">HauzKhas Village</span>
-					
-							<div className="a-pro-btn">
-								<span className="btn-in">₹50</span>
-								<span className="btn-wa"><a href="#"><span className="fa fa-phone-square"></span> Call</a></span>
-								<span className="btn-suc"><a href="#"><span className="fa fa-phone-square"></span> Chat</a></span>
-								{/*<span className="btn-suc"><a href="astrloger-book.html" ><span className="glyphicon glyphicon-calendar"></span> Chat</a></span>*/}
+				<div className="col-md-10">
+					<div className="card card-ct">
+						<div className="card-body">
+							<div className="astloger-widget">
+								<div className="doc-info-left">
+									<div className="astloger-img">
+										<img src="https://astrotalk.com/assets/astro/771.jpg" className="img-fluid" alt="User Image" />
+									</div>
+									<div className="doc-info-cont">
+										<h4 className="doc-name">Acharya Balwant Sharma</h4>
+										<p className="doc-speciality">Tarot Card Reading,Numerology,Vaastu,Prashna Kundali</p>
+										<p className="doc-speciality">Language : Hindi,Gujarati</p>
+										<p className="doc-speciality">Experience : 15 Year</p>
+										<p className="doc-speciality"><i className="fa fa-map-marker"></i> Location : Gurgaon, India</p>
+										<div className="rating">
+											<i className="fa fa-star filled"></i>
+											<i className="fa fa-star filled"></i>
+											<i className="fa fa-star filled"></i>
+											<i className="fa fa-star filled"></i>
+											<i className="fa fa-star"></i>
+											<span className="d-inline-block average-rating">(35)</span>
+										</div>
+										
+										
+									</div>
+								</div>
+								<div className="doc-info-right">
+									<div className="clini-infos">
+										<ul>
+											<li><i className="fa fa-comment"></i> 35 Feedback</li>
+											<li><i className="fa fa-map-marker"></i>Gurgaon, INDIA</li>
+											<li><i className="fa fa-rupee"></i> 100 per hour </li>
+										</ul>
+									</div>
+									<div className="astloger-action">
+										<a href="chat.html" className="btn btn-white msg-btn">
+											<i className="fa fa-comment"> Chat</i>
+										</a>
+										<a href="javascript:void(0)" className="btn btn-white call-btn" data-toggle="modal" data-target="#voice_call">
+											<i className="fa fa-phone"> Call</i>
+										</a>
+										<a href="javascript:void(0)" className="btn btn-white call-btn" data-toggle="modal" data-target="#video_call">
+											<i className="fa fa-tv"> Video</i>
+										</a>
+									</div>
+								</div>
 							</div>
-							
 						</div>
 					</div>
 				</div>
-				
-				<div className="col-md-6">
-					<div className="a-t-title">
-						<h4>Personal Statement</h4>
-					</div>
-					<div className="a-t-inner">
-					<p>Quotes from Vikas Nautiyal “Nothing is final in the world, whether it is sorrow or happiness, whether it is success or failure. It is god's blessing to me that I am able to help you in overcoming the situation. <br/>If I get success to make you out of a tougher situation than it is my pleasure"....... He is a strong believer that the actions of our previous lives determine the present. <br/>Everyy event of the present life which is beyond our control is predetermined. A good environment at work, a good work place, a good doer and all the resources and efforts of doer are if not able to perform a work, then only the ‘Fate’ is the reason for non-completion and failure.<br/> – 18/11/42 Shloka of Bhagwat Geeta. Can any astrological remedy change the course of events? He says, Yes, to a great extent, but only where the right remedy is given which may be either Mantra or Pujas or Gemstones. <br/>He strongly believes that there are only two things that exist in the whole universe and that are namely<br/> 1) vibration/sound and <br/>2) energy, which is scientifically true also. Mantras are nothing but vibrations of different kinds and Gemstones provide cosmic energy through their divine rays which is required by an individual. <br/>He spent many years in detailed research and study of Astronomy, Astrology, Numerology, Gem-therapy, and Mantras etc. <br/>His sole motto was to solve the problems being faced by the people in different walks of life. To quote a few such as, that the girl’s family is very rich but marriage is not taking place, an advocate not able to solve his own problem, a doctor not able to treat his own child, etc.<br/> Since the age of around 30 he started providing astro-consultations solving the problems with innumerable correct prediction. During the last 8 years he has gone through thousands of horoscopes and has imparted astrological consultations to foreigners, NRI people and locals in India also. <br/>He has deep studies of this subject and has great expertise over various matters such as Health, Money, Shares and Investments, Travels, Education, Progeny, Litigation, Business and Corporate Matters, Longevity, Career, Marriage and related matters viz. Romance and Divorce. etc. <br/>His predictive system includes Vimshottari Dasha and Transit of planets in various Signs and Nakshatra. By the Grace of God, he has a good command over ‘Birth Time Rectification’ since many people who do not know their birth time still wish to know their future to execute their plans. For this he requires few major events of life which are beyond the control of a native such as date of birth of a child, marriage, education and percentage attained in the exams etc. <br/>He also involves the Ruling Planets at the time of rectifications. At times, it happens that an individual comes with a single peculiar problem in mind and desires an answer from the astrologer. The technique adopted at the time of answering is ‘Horary Astrology’. MR.Nautiyal has good command over this technique and thousands have been benefited by this service.   less</p>
-					</div>
-				</div>
-				
-				
-				<div className="col-md-6">
-					<div className="a-t-title">
-						<h4>Write Reviews</h4>
-					</div>
-					<div className="a-t-inner">
-						<div className="row row-f">
-							<h5>Submit a review for Vikas Nautiyal</h5>
-							<p>Your feedback matters!</p>
-							<form className="submit-fr">
-								<div className="col-md-6">
-									<div className="form-group">
-										<label for="name">Full Name</label>
-										<input type="text" className="form-control" placeholder="Your Name" required="" />
-									</div>
-								</div>
-								<div className="col-md-6">
-									<div className="form-group">
-										<label for="email">Email address:</label>
-										<input type="email" className="form-control" placeholder="Your Emial ID" required="" />
-									</div>
-								</div>
-								<div className="col-md-12">
-									<div className="form-group">
-										<label for="message">Your message</label>
-										<textarea className="form-control" rows="5" id="comment" required="" placeholder="Please enter your feedback here..."></textarea>
-									</div>
-									
-									<div className="lead">
-									<div id="stars" className="starrr"></div>
-										<div className="rat-infd">You gave a rating of <span id="count">0</span> star(s)</div>
-									</div>
-								</div>
-								<div className="col-md-12">
-									<button className="theme-btn" type="submit" name="submit-form">Submit Reviews</button>
+
+				<div className="col-md-10">
+					<div className="tab-v1">
+						<nav>
+						<div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
+						<a class="nav-item nav-link nav-link-bg active" id="nav-recharge-tab" data-toggle="tab" href="#nav-recharge" role="tab" aria-controls="nav-recharge" aria-selected="true">Overview</a>
+						<a class="nav-item nav-link nav-link-bg" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Reviews</a>
+						<a class="nav-item nav-link nav-link-bg" id="nav-about-tab" data-toggle="tab" href="#nav-call" role="tab" aria-controls="nav-call" aria-selected="false">Business Hours</a>
+						</div>
+
+						</nav>
+
+						<div className="tab-content tab-content-astro" id="nav-tabContent">
+							<div className="tab-pane fade active show" id="nav-recharge" role="tabpanel" aria-labelledby="nav-recharge-tab">
+								<div className="widget about-widget">
+									<h4 className="widget-title">About Me</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 								</div>
 
-							</form>
+								<div className="service-list">
+									<h4>Services</h4>
+										<ul className="clearfix">
+											<li>Tarot Card Reading </li>
+											<li>Numerology</li>
+											<li>Vaastu</li>
+											<li>Prashna Kundali</li>
+										</ul>
+								</div>
+
+
+
+							</div>
+
+							<div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+								<div className="write-review">
+									<h4>Write a review for <strong>Acharya Balwant Sharma</strong></h4>
+									<form>
+										<div className="form-group">
+											<label>Review</label>
+											<div className="star-rating">
+												<input id="star-5" type="radio" name="rating" value="star-5" />
+												<label for="star-5" title="5 stars">
+													<i className="active fa fa-star"></i>
+												</label>
+												<input id="star-4" type="radio" name="rating" value="star-4" />
+												<label for="star-4" title="4 stars">
+													<i className="active fa fa-star"></i>
+												</label>
+												<input id="star-3" type="radio" name="rating" value="star-3" />
+												<label for="star-3" title="3 stars">
+													<i className="active fa fa-star"></i>
+												</label>
+												<input id="star-2" type="radio" name="rating" value="star-2" />
+												<label for="star-2" title="2 stars">
+													<i className="active fa fa-star"></i>
+												</label>
+												<input id="star-1" type="radio" name="rating" value="star-1" />
+												<label for="star-1" title="1 star">
+													<i className="active fa fa-star"></i>
+												</label>
+											</div>
+										</div>
+										<div className="form-group">
+											<label>Title of your review</label>
+											<input className="form-control" type="text" placeholder="If you could say it in one sentence, what would you say?" />
+										</div>
+										<div className="form-group">
+											<label>Your review</label>
+											<textarea id="review_desc"  rows="5" className="form-control"></textarea>
+										  
+										  <div className="d-flex justify-content-between mt-3"><small className="text-muted"><span id="chars">100</span> characters remaining</small></div>
+										</div>
+										<hr/>
+										<div className="form-group">
+											<div class="terms-accept">
+												<div class="custom-checkbox">
+												   <input type="checkbox" id="terms_accept" />
+												   <label for="terms_accept">I have read and accept <a href="#">Terms &amp; Conditions</a></label>
+												</div>
+											</div>
+										</div>
+										<div className="submit-section">
+											<button type="submit" className="btn btn-warning submit-btn">Add Review</button>
+										</div>
+									</form>	
+								</div>
+							</div>
+
+							<div className="tab-pane fade" id="nav-call" role="tabpanel" aria-labelledby="nav-call-tab">
+								<div className="widget business-widget">
+									<div className="widget-content">
+										<div className="listing-hours">
+											<div className="listing-day current">
+												<div className="day">Today <span>5 May 2020</span></div>
+												<div className="time-items">
+													<span className="open-status"><span className="badge bg-success-light">Open Now</span></span>
+													<span className="time">07:00 AM - 09:00 PM</span>
+												</div>
+											</div>
+											<div className="listing-day">
+												<div className="day">Monday</div>
+												<div className="time-items">
+													<span className="time">07:00 AM - 09:00 PM</span>
+												</div>
+											</div>
+											<div className="listing-day">
+												<div className="day">Tuesday</div>
+												<div className="time-items">
+													<span className="time">07:00 AM - 09:00 PM</span>
+												</div>
+											</div>
+											<div className="listing-day">
+												<div className="day">Wednesday</div>
+												<div className="time-items">
+													<span className="time">07:00 AM - 09:00 PM</span>
+												</div>
+											</div>
+											<div className="listing-day">
+												<div className="day">Thursday</div>
+												<div className="time-items">
+													<span className="time">07:00 AM - 09:00 PM</span>
+												</div>
+											</div>
+											<div className="listing-day">
+												<div className="day">Friday</div>
+												<div className="time-items">
+													<span className="time">07:00 AM - 09:00 PM</span>
+												</div>
+											</div>
+											<div className="listing-day">
+												<div className="day">Saturday</div>
+												<div className="time-items">
+													<span className="time">07:00 AM - 09:00 PM</span>
+												</div>
+											</div>
+											<div className="listing-day closed">
+												<div className="day">Sunday</div>
+												<div className="time-items">
+													<span className="time"><span className="badge bg-danger-light">Closed</span></span>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+
+							</div>
 						</div>
 					</div>
 				</div>
-				
 				
 			</div>
 		</div>
